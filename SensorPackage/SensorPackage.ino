71bdd9f107449ca8f7b4a72b211cde172d73c695
David <56525307+Mr-Hansen-Orewa@users.noreply.github.com>
2024-04-07 13:23:05 +1200

    Update SensorPackage.ino
    teacher notes added

--- SensorPackage/SensorPackage.ino
+++ SensorPackage/SensorPackage.ino
@@ -1,34 +1,36 @@
 //libraries
+ //TEACHER COMMENTS I would have expected the openlog library as well so you could have your SDcard object work
 #include <SparkFun_TMP117.h>
 #include <SparkFun_TMP117_Registers.h>
 #include <SparkFun_LPS25HB_Arduino_Library.h>
 
-
+//objects like sdcard and sensor board
 LPS25HB sensor;  // Create an object of the LPS25HB class
-
 OpenLog SDcard; //creating a log of all our measurements
 //constants
 
-//objects like sdcard and sensor board
+
 void setup() {
 
   Wire.begin();
   sensor.begin();  // Begin links an I2C port and I2C address to the sensor, sets an I2C speed, begins I2C on the main board, and then sets default settings
-
   Serial.begin (9600);//setting up serial monitor
+   //TEACHER COMMENTS might want that SDcard to begin hereish
   
-}if(sensor.isConnected() == false) {
+ //TEACHER COMMENTS what is this chunk of code doing?  
+if(sensor.isConnected() == false) {
 Serial.println("Sensor isn't reading.");
 
 }
-
+}
 void loop() {
-  // put your main code here, to run repeatedly:
+ //TEACHER COMMENTS so what does this chunk of code do?
   Serial.print("Current pressure : ");
   Serial.println(sensor.getPressure_hPa());  // Get the pressure reading in hPa
   Serial.print("Current Temperature : ");
   Serial.println(sensor.getTemperature_degC(°C));
 
+   //TEACHER COMMENTS what about these lines of code?
   SDcard.print("Current pressure : ");
   SDcard.println(sensor.getPressure_hPa());  // Get the pressure reading in hPa
   SDcard.print("Current Temperature : ");
