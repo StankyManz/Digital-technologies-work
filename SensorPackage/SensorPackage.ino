c3e70c83bf4d8fa10b6c6e6b9aeed18d1110e303
StankyManz <158523635+StankyManz@users.noreply.github.com>
2024-05-13 13:32:40 +1200

    Updated

--- SensorPackage/SensorPackage.ino
+++ SensorPackage/SensorPackage.ino
@@ -1,40 +1,58 @@
-//libraries
- //TEACHER COMMENTS I would have expected the openlog library as well so you could have your SDcard object work
-#include <SparkFun_TMP117.h>
-#include <SparkFun_TMP117_Registers.h>
-#include <SparkFun_LPS25HB_Arduino_Library.h>
+// setting up library's for the code 
+#include <Wire.h>
+#include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SparkFun_LPS25HB_Arduino_Library.h> 
 
-//objects like sdcard and sensor board
-LPS25HB sensor;  // Create an object of the LPS25HB class
-OpenLog SDcard; //creating a log of all our measurements
-//constants
+//TEACHER COMMENTS what do these lines of code do?
+LPS25HB Sensor;
+Sd2Card card;// create instance 
+SdVolume volume;// create instance 
+SdFile root;// create instance 
+OpenLog sdCard; // create instance 
 
+//TEACHER COMMENTS what about this one?
+const String Bottle_rocket_reading = "SD.sensor_reading_storage";
 
 void setup() {
-
+ Serial.begin(9600);
   Wire.begin();
-  sensor.begin();  // Begin links an I2C port and I2C address to the sensor, sets an I2C speed, begins I2C on the main board, and then sets default settings
-  Serial.begin (9600);//setting up serial monitor
-   //TEACHER COMMENTS might want that SDcard to begin hereish
-  
- //TEACHER COMMENTS what is this chunk of code doing?  
-if(sensor.isConnected() == false) {
-Serial.println("Sensor isn't reading.");
+Sensor.begin(); 
+sdCard.begin();
 
-}
-}
-void loop() {
- //TEACHER COMMENTS so what does this chunk of code do?
-  Serial.print("Current pressure : ");
-  Serial.println(sensor.getPressure_hPa());  // Get the pressure reading in hPa
-  Serial.print("Current Temperature : ");
-  Serial.println(sensor.getTemperature_degC(°C));
 
-   //TEACHER COMMENTS what about these lines of code?
-  SDcard.print("Current pressure : ");
-  SDcard.println(sensor.getPressure_hPa());  // Get the pressure reading in hPa
-  SDcard.print("Current Temperature : ");
-  SDcard.println(sensor.getTemperature_degC(°C));
+ 
+ //TEACHER COMMENTS what does this chunk of code do? adjust its error message to something you would say
+  if (Sensor.isConnected() == false) // The library supports some different error codes such as "DISCONNECTED"
+  {
+    Serial.println("LPS25HB disconnected. Reset the board to try again.");     // Alert the user that the device cannot be reached
+    Serial.println("Are you using the right Wire port and I2C address?");      // Suggest possible fixes
+    Serial.println("See Example2_I2C_Configuration for how to change these."); // Suggest possible fixes
+    Serial.println("");
+    while (1)
+      ;
+  }
+//TEACHER COMMENTS any sdcard checks to do?
 
-  delay(100); //every 0.1 secounds the sensors take and record another reading
+ 
 }
+
+void loop()
+{
+ //TEACHER COMMENTS what does this chunk of code do?
+  Serial.print("current preassure: ");
+  Serial.print(Sensor.getPressure_hPa());  // getting the tempature reading in hPa
+  sdCard.print(", Current tempature (C degrees): ");
+  Serial.println(Sensor.getTemperature_degC()); // geting the tempature in degrees 
+
+ //TEACHER COMMENTS what about these? check if they make a file that looks like the csv example on classroom
+    sdCard.print("current preassure: ");
+  sdCard.print(Sensor.getPressure_hPa());  // getting the tempature reading in hPa
+  sdCard.print(", Current tempature (C degrees): ");
+  sdCard.println(Sensor.getTemperature_degC()); // geting the tempature in degrees 
+
+delay(40); // waiting 40 miliseconds so that we can goet a good reading like 16, 14, 11, 9 insted of 16, 16, 16, 16, 14, 14, 14, 14, 14, 14, 
+}
+
+void launchDirectory
