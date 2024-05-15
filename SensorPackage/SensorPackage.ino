690300c77c912320dd7d19d1be5a114bbc5cf76e
StankyManz <158523635+StankyManz@users.noreply.github.com>
2024-05-16 00:51:37 +1200

    Update SensorPackage.ino

--- SensorPackage/SensorPackage.ino
+++ SensorPackage/SensorPackage.ino
@@ -1,94 +1,96 @@
-// setting up library's for the code
+ //setting up library's for the code:
 #include <Wire.h>
 #include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
 #include <SparkFun_LPS25HB_Arduino_Library.h>
 
+ //suppling constant values with names for SDcard debugging purposes:
 #define STATUS_SD_INIT_GOOD 0
 #define STATUS_LAST_COMMAND_SUCCESS 1
 #define STATUS_LAST_COMMAND_KNOWN 2
 #define STATUS_FILE_OPEN 3
 #define STATUS_IN_ROOT_DIRECTORY 4
 
-//Creating instances of Sensor and SD Card
+ //creating instances of Sensor and SD Card:
 LPS25HB Sensor;
 OpenLog sdCard;
 
-const String FILENAME = "sensorLogCG.txt";
+const String FILENAME = "sensorLogCG.txt"; //naming file on SDcard
 
-void setup() {
+void setup() { //setup
 
-  Serial.begin(9600);
+  Serial.begin(9600);//setting up serial monitor
+
+ //setting up sensor and SDcard:
   Wire.begin();
   Sensor.begin();
   sdCard.begin();
 
-  sdCard.append(FILENAME);
+  sdCard.append(FILENAME); // naming file on SDcard
 
-  Serial.println("Pressure , Temperature");
-  sdCard.println("Pressure , Temperature");
-  //TEACHER COMMENTS what does this chunk of code do? adjust its error message to something you would say
-  if (Sensor.isConnected() == false) {
-    Serial.println("LPS25HB disconnected. Reset the board to try again.");      // Alert the user that the device cannot be reached
-    Serial.println("Are you using the right Wire port and I2C address?");       // Suggest possible fixes
-    Serial.println("See Example2_I2C_Configuration for how to change these.");  // Suggest possible fixes
-    Serial.println("");
+Serial.println("NEW Temperature and Pressure readings"); //setting a checkmark so that new readings can be identified from older readings
+ //creating headers for the temperature and pressure values:
+  Serial.println("Pressure , Temperature"); //print for serial monitor
+  sdCard.println("Pressure , Temperature"); //print for SDcard
+
+ //testing if sensor is connected/working:
+  if (Sensor.isConnected() == false) { //if the sensor is not connected
+    Serial.println("sensor is disconnected"); //print in the serial monitor that the sensor is disconnected
   }
 
-  byte status = sdCard.getStatus();
-  //TEACHER COMMENTS any sdcard checks to do? too dooo
-Serial.print("Status byte: 0x");
-  if(status < 0x10) { 
-  Serial.print("0");
-  Serial.println(status, HEX);
+  byte sdStatus = sdCard.getStatus(); //getting the status from sdCard and giving it to the variable sdStatus
+  if(sdStatus < 0x10) { //if the sdcard status meets a certain criteria
+  Serial.println(sdStatus, HEX); //print the SDcard status as a hexidecimal into serial monitor
   }
-  if(status == 0xFF) {
-    Serial.println("OpenLog failed to respond. Freezing.");
-    while(1); //Freeze!
+  if(sdStatus == 0xFF) { //if the sdcard status meets a certain criteria
+    Serial.println("SDcard not responding"); //print the SDcard is not responding into serial monitor
   }
 
-  if(status & 1<<STATUS_SD_INIT_GOOD) {
-    Serial.println("SD card is good");
-   } else {
-    Serial.println("SD init failure. Is the SD card present? Formatted?");
+  if(sdStatus & 1<<STATUS_SD_INIT_GOOD) { //if the sdcard status meets a certain criteria
+    Serial.println("SD card is working"); //print the SDcard is working into serial monitor
+   } else { //if the sdcard status does not meet criteria
+    Serial.println("SDcard Failiure"); //print SDcard Failiure into serial monitor
    }
-  if(status & 1<<STATUS_IN_ROOT_DIRECTORY) {
-    Serial.println("Root directory open");
-   } else {
-    Serial.println("Root failed to open. Is SD card present? Formatted?");
+  if(sdStatus & 1<<STATUS_IN_ROOT_DIRECTORY) { //if the sdcard status meets a certain criteria
+    Serial.println("Root directory is open"); //print the Root directory is open into serial monitor
+   } else { //if the sdcard status does not meet criteria
+    Serial.println("Root has failed to open"); //print the Root has failed to open into serial monitor
    }
-  if(status & 1<<STATUS_FILE_OPEN) {
-    Serial.println("Log file open and ready for recording");
-   } else {
-    Serial.println("No log file open. Use append command to start a new log.");
+  if(sdStatus & 1<<STATUS_FILE_OPEN) { //if the sdcard status meets a certain criteria
+    Serial.println("File is open"); //print the file is open into serial monitor
+   } else { //if the sdcard status does not meet criteria
+    Serial.println("file cannot be opened"); //print the file cannot be opened into serial monitor
    }
-   Serial.println("NEW Temperature and Pressure readings");
 } 
 
-void loop() {
+void loop() { //main loop that determines whether the data is printed to SDcard or serial monitor
 
-  byte status = sdCard.getStatus();
-
-  if (status != 0xFF) {
-  launchSD(true); //true = SDcard is connected
-  } else {
-  launchSD(false); //false = SDcard is not connected
+  byte sdStatus = sdCard.getStatus(); //getting the status from sdCard and giving it to the variable sdStatus
+  if (sdStatus != 0xFF) { //if the SDcard status does not meet a certain criteria meaning SDcard is connected
+  launchSD(true); //go to LaunchSD with true = SDcard is connected
+  } else { //if the sdcard status meets criteria meaning SDcard is not connected
+  launchSD(false); //go to LaunchSD with false = SDcard is not connected
   }
 }
 
-void launchSD(bool launchSD) {
+void launchSD(bool launchSD) { //method recieving a boolean value(true or false)
 
-  if (launchSD == true){
-  delay(200);
-  Serial.println("SDcard Printing");
-  sdCard.print(Sensor.getPressure_hPa());  // getting the tempature reading in hPa
-  sdCard.print (", (C)");
-  sdCard.println(Sensor.getTemperature_degC());  // geting the tempature in degrees
-  sdCard.syncFile();
-  } else {
-    //TEACHER COMMENTS what does this chunk of code do?
-    Serial.print(Sensor.getPressure_hPa());  // getting the tempature reading in hPa
-    Serial.print (", (C)");
-    Serial.println(Sensor.getTemperature_degC());  // geting the tempature in degrees
-    delay(200);
+  if (launchSD == true){ //if recieved true
+  //SDcard printing:
+  Serial.println("SDcard Printing"); //print SDcard printing into serial monitor
+  Serial.print("(hPa)"); //labelling value as hPa
+  sdCard.print(Sensor.getPressure_hPa()); //printing pressure in hPa to SDcard
+  sdCard.print (" , "); //dividing the two values
+  Serial.print("(C)"); //labelling value as degrees C
+  sdCard.println(Sensor.getTemperature_degC()); //printing Temperature in degrees C to SDcard
+  sdCard.syncFile(); //sending information to SDcard
+  delay(200); //delaying by 200ms to get a more controllable amount of variables
+  } else { //if recieved false
+  //Serial monitor printing:
+    Serial.print("(hPa)"); //labelling value as hPa
+    Serial.print(Sensor.getPressure_hPa()); //printing pressure in hPa to serial monitor
+    Serial.print (" , "); //dividing the two values
+    Serial.print("(C)"); //labelling value as degrees C
+    Serial.println(Sensor.getTemperature_degC()); //printing Temperature in degrees C to serial monitor
+    delay(200); //delaying by 200ms to get a more controllable amount of variables
   }
 }
